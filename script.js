0f21f62bccebe8d4318cf55f30072c360ab97b0e
Lawrence Zheng <lxzheng@umich.edu>
2018-11-05 22:05:47 -0500

    Added fade in effect
    Added fade in effect for background

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 (function() {
   // Parallax scrolling
   // Tutorial: https://medium.com/@PatrykZabielski/how-to-make-multi-layered-parallax-illustration-with-css-javascript-2b56883c3f27
+
+  window.onload = function(){
+    layers = document.querySelectorAll("[data-type='parallax']");
+    for (i = 0, len = layers.length; i < len; i++) {
+      layer = layers[i];
+      translate3d = 'translate3d(0, 30px, 0)';
+      layer.style['-webkit-transform'] = translate3d;
+      layer.style['-moz-transform'] = translate3d;
+      layer.style['-ms-transform'] = translate3d;
+      layer.style['-o-transform'] = translate3d;
+      layer.style.transform = translate3d;
+    }
+    $("#bg-front").fadeIn(1000)
+    window.setTimeout($("#bg-back").fadeIn(1500), 1000)
+      
+  };
+
   window.addEventListener('scroll', function(event) {
     var depth, i, layer, layers, len, movement, topDistance, translate3d;
     topDistance = this.pageYOffset;
