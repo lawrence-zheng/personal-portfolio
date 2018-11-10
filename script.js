efd00b5cd2d212bc4a29415f496ac65169cc9fdb
Lawrence Zheng <lxzheng@umich.edu>
2018-11-09 20:29:23 -0500

    Small adjustments for mobile
    Adjustments to mobile background hide

--- script.js
+++ script.js
@@ -3,18 +3,23 @@
   // Tutorial: https://medium.com/@PatrykZabielski/how-to-make-multi-layered-parallax-illustration-with-css-javascript-2b56883c3f27
 
   window.onload = function(){
-    layers = document.querySelectorAll("[data-type='parallax']");
-    for (i = 0, len = layers.length; i < len; i++) {
-      layer = layers[i];
-      translate3d = 'translate3d(0, 30px, 0)';
-      layer.style['-webkit-transform'] = translate3d;
-      layer.style['-moz-transform'] = translate3d;
-      layer.style['-ms-transform'] = translate3d;
-      layer.style['-o-transform'] = translate3d;
-      layer.style.transform = translate3d;
-    }
-    $("#bg-front").fadeIn(1000)
-    $("#bg-back").fadeIn(1500)
+    if( window.screen.availHeight < 800 ) {
+        console.log("mobile");
+    } else { 
+      layers = document.querySelectorAll("[data-type='parallax']");
+      for (i = 0, len = layers.length; i < len; i++) {
+        layer = layers[i];
+        translate3d = 'translate3d(0, 30px, 0)';
+        layer.style['-webkit-transform'] = translate3d;
+        layer.style['-moz-transform'] = translate3d;
+        layer.style['-ms-transform'] = translate3d;
+        layer.style['-o-transform'] = translate3d;
+        layer.style.transform = translate3d;
+      }
+      $("#bg-front").fadeIn(1000)
+      $("#bg-back").fadeIn(1500)
+      }
+
       
   };
 
